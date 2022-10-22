a180c2bbd2f122c4971033c545076c3a7721c208
saelsam <saelsamuelrude@gmail.com>
2022-10-23 01:54:12 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+alert("Selamat Datang di Website Wisata Toraja")
+
+function darkmode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode-body");
+    var content = document.getElementById("darkMode");
+}
+
+const judul = document.getElementById('judul');
+judul.style.color = 'white';
+judul.style.fontStyle = 'italic'
+
+
+
+
